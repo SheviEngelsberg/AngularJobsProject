9e52629e831f60425d249075475fb67054cbbd39
SheviEngelsberg <s4161823@gmail.com>
2024-04-10 11:27:58 +0300

    server side

---
+++ server/Jobs-Server/WebApplication1/WebApplication1/Controllers/JobController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Interfaces;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+        IJobService jobService;
+        public JobController(IJobService jobService)
+        {
+            this.jobService = jobService;
+        }
+
+        [HttpGet("GetAllJobs")]
+        public ActionResult GetAllJobs()
+        {
+            return Ok(jobService.GetAllJobs());
+        }
+
+    }
+}
---
+++ server/Jobs-Server/WebApplication1/WebApplication1/Services/UserService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using WebApplication1.Interfaces;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public class UserService: IUserService
+    {
+        List<User> users = new List<User>()
+        {
+            new User { Id = 1, UserName = "Shevi", Password = "1111", JobSearchField = JobFields.Secretariat },
+            new User { Id = 2, UserName = "Mali", Password = "2222", JobSearchField = JobFields.Architecture },
+            new User { Id = 3, UserName = "Adam", Password = "3333", JobSearchField = JobFields.ComputerProgramming },
+        };
+        public User GetUser( string userName, string password)
+        {
+            User user=users.Find(user=> user.UserName == userName && user.Password == password);
+            if(user==null)
+                return null;
+            return user;
+        }
+    }
+}
