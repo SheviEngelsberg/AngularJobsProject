08c661581dd4b05b084ce2d10ff6ff5a605837b2
SheviEngelsberg <s4161823@gmail.com>
2024-04-10 14:27:19 +0300

    Access to the server and add navigation

--- AngularClientSideJobsProject/src/app/app-routing.module.ts
+++ AngularClientSideJobsProject/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponentComponent } from './components/main-component/main-component.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: MainComponentComponent},
+  {path: "login", component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AngularClientSideJobsProject/src/app/app.module.ts
+++ AngularClientSideJobsProject/src/app/app.module.ts
@@ -6,21 +6,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { MainComponentComponent } from './components/main-component/main-component.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    MainComponentComponent
+    MainComponentComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule,
+    RouterModule
+
   ],
   providers: [
     provideClientHydration()
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+}
